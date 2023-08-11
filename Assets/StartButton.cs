15803b84999efe3d24b51c6db51c196b7f3dc126
yamada-Syotaro <134474129+yamada-Syotaro@users.noreply.github.com>
2023-08-11 18:58:26 +0900

    スタートボタンの追加

---
+++ Assets/StartButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
